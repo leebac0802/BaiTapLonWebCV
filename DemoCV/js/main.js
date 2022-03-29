4efd074f4f5961af1797e7e3623940b14654a0df
backoi <2051063807@e.tlu.edu.vn>
2022-03-29 23:32:39 +0700

    them js

--- DemoCV/js/main.js
+++ DemoCV/js/main.js
@@ -1,1 +1,20 @@
 // alert('hello')
+$(document).ready(function () {
+    $(".slideImgs").isotope({
+    // options
+    itemSelector: "img",
+    layoutMode: "fitRows",
+});
+    const wimg = $("img").width();
+    $(".slideImgs").forEach((val) => {
+      val.css("height", `${wing}px`)
+});
+    slideImgs
+})
+$(".controls").on("click", "button",function(){
+    const filter = $(this).data("filter")
+    $(".slideImgs").isotope({ filter })
+    $("button").removeClass()
+    $(this).addClass("active-btn")
+})
+
