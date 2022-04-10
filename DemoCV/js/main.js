94d7e02992d320ab473bb0b8a8edce65fb71b63c
Giang28082002 <2051063556@e.tlu.edu.vn>
2022-04-10 14:58:36 +0700

    head for CV

--- DemoCV/js/main.js
+++ DemoCV/js/main.js
@@ -1,1 +1,17 @@
-alert('hello')
+var index = 1;
+changeImage = function(){
+    var imgs = ["https://cafefcdn.com/2019/8/22/edf-tempo-couleur-de-demain-la-edf-tempo-historique-couleur-jour-et-demain-15664469984471689659056.jpg","http://figarigroup.com/wp-content/uploads/2018/04/annie-spratt-604131-unsplash-1024x702.jpg", "https://bigdata-vn.com/wp-content/uploads/2021/10/Hinh-anh-goc-lam-viec-dep-doc-dao-va-day.jpg"];
+    document.getElementById('img').src = imgs[index];
+    index++;
+    if(index==3){
+        index = 0;
+    }  
+}   
+setInterval(changeImage,5000); 
+let menu = document.getElementById('menu')
+Menu = () =>{
+    menu.classList.remove('disappears')
+}
+Close = () =>{
+    menu.classList.add('disappears')
+}
